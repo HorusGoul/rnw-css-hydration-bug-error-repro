e341a77e8b0c446137c90e32aa5fb4feea20bac2
Horus Lugo <horusgoul@gmail.com>
2022-07-02 12:12:02 +0200

    chore: vercel

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,11 @@ module.exports = {
   publicPath: "/build/",
   serverBuildDirectory: "build",
   devServerPort: 8002,
-  ignoredRouteFiles: [".*"],
   serverDependenciesToBundle: [/^react-native.*/],
+  serverBuildTarget: "vercel",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  ignoredRouteFiles: ["**/.*"],
 };
