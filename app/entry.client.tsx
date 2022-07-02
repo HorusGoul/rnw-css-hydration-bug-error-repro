dc15f6c2501147ec4335f4ec2054f8bfe266a43b
Horus Lugo <horusgoul@gmail.com>
2022-07-02 11:50:21 +0200

    bug reproduction with logs

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,15 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
+import { renderToStaticMarkup } from "react-dom/server";
 import { AppRegistry } from "react-native";
 import { ReactNativeStylesContext } from "./rn-styles";
+
+const serverStyleMarkup = document.getElementById(
+  "react-native-stylesheet"
+)?.outerHTML;
+
+console.log("%c Server Markup", "color: red");
+console.info(serverStyleMarkup);
 
 const App = () => <RemixBrowser />;
 
@@ -9,6 +17,12 @@ AppRegistry.registerComponent("App", () => App);
 
 // @ts-ignore
 const { getStyleElement } = AppRegistry.getApplication("App");
+
+const clientStyleMarkup = renderToStaticMarkup(getStyleElement());
+
+// console log colored red
+console.log("%c Client Markup", "color: red");
+console.info(clientStyleMarkup);
 
 hydrateRoot(
   document,
